aa4174d9d05519fbb015788319a84e4a6d10e9eb
Lucas Davila <nightdavila@gmail.com>
2023-07-17 23:29:04 -0300

    Aprendido sobre Objetos e Interfaces no typescript

--- Fundamentos/app.ts
+++ Fundamentos/app.ts
@@ -1,5 +1,41 @@
-const soma = (a:number, b:number) => {
-  console.log(a + b)
+// let b: string = 'b'
+
+// let m: string | number = 2
+
+// Objetos
+
+// Interface
+interface Pessoa {
+  nome: string,
+  idade: number,
+  profissao?: string
 }
 
-soma(1, 2)
+// Objeto pessoa sem a opcao opcional
+const pessoa: Pessoa = {
+  nome: 'Lucas',
+  idade: 32
+}
+
+// Objeto pessoa com a opcao opcional
+const outraPessoa: Pessoa = {
+  nome: 'Paulo',
+  idade: 25,
+  profissao: 'Desenvolvedor'
+}
+
+// Array de objetos do tipo "Pessoa "
+const arrayPessoas: Array<Pessoa> = [
+  pessoa,
+  outraPessoa
+]
+
+// Array do tipo number (primitivo)
+const arrayNum: number[] = [
+  1,2,3
+]
+
+// Array do tipo string (primitivo)
+const arrayString: Array<string> = [
+  'a', 'b', 'c'
+]
